02054e0290b9e9cd621d65df0a90b48213b048e2
Md Nayab Alam <alamnayab718@gmail.com>
2026-09-15 14:45:49 +0530

    Check Razorpay environment variables

--- server/services/razorpay.service.js
+++ server/services/razorpay.service.js
@@ -1,10 +1,15 @@
-import dotenv from "dotenv"
-dotenv.config()
-import Razorpay from "razorpay"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import Razorpay from "razorpay";
+
+console.log("RAZORPAY_KEY_ID exists:", !!process.env.RAZORPAY_KEY_ID);
+console.log("RAZORPAY_KEY_SECRET exists:", !!process.env.RAZORPAY_KEY_SECRET);
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
-export default razorpay
+export default razorpay;
