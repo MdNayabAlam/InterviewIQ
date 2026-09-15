804e6481e9bcb791bdb4804687af7bd3414dd49a
Md Nayab Alam <alamnayab718@gmail.com>
2026-09-15 18:22:53 +0530

    Fix production CORS

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://interview-iq-livid.vercel.app",
     credentials: true
 }));
 
